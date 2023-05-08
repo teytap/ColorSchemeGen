0ccf16e7c3b9f53cc40149ecd347f0b4e33a6f83
Mehtap Tataroğlu <teytap@gmail.com>
2023-05-08 22:48:13 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,56 @@
+const colorForm = document.getElementById("color-form");
+const colorInput = document.getElementById("color-input");
+const modeInput = document.getElementById("mode-input");
+const getColorsBtn = document.getElementById("get-colors-btn");
+const colorsContainer = document.getElementById("colors-container");
+
+let mode = modeInput.value;
+let seedColor = colorInput.value.substring(1);
+// document.addEventListener("click", function (e) {
+//   if ((e.target.id = dataset.color)) {
+//     console.log("done");
+//   }
+// });
+
+colorInput.addEventListener("change", (e) => {
+  seedColor = e.target.value.substring(1);
+});
+modeInput.addEventListener("change", (e) => {
+  mode = e.target.value;
+});
+
+colorForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  render();
+});
+
+function render() {
+  let colorsHtml = "";
+  fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const colors = data.colors;
+      let hexColor = "";
+      colors.map((color) => {
+        hexColor = color.hex.value;
+        colorsHtml += `<div><div class="color-block" style="background:${hexColor}"
+         data-color="${hexColor}" onclick="copyColor(this.dataset.color)"></div>
+         <p class="color-hex">${hexColor}</p></div>`;
+      });
+      colorsContainer.innerHTML = colorsHtml;
+      document.querySelector("h1").style.color = hexColor;
+    });
+}
+
+function copyColor(color) {
+  navigator.clipboard.writeText(color);
+
+  document.getElementById(
+    "message"
+  ).textContent = `Copied to clipboard color ${color}`;
+  setTimeout(function () {
+    document.getElementById("message").textContent = "";
+  }, 3000);
+}
+
+render();
